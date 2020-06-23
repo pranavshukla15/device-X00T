86e813875154a871a14931a9587d423103fec03e
Pranavshukla15 <38955291+pranavshukla15@users.noreply.github.com>
2020-06-23 11:42:10 +0530

    Ancient

--- device.mk
+++ device.mk
@@ -344,7 +344,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
-#    $(LOCAL_PATH)/overlay-mokee
+#    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
